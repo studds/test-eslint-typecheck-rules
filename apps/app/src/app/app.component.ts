42e3caa40340ae79db65e58921739794fbefa63a
Daniel Studds <studds@fastmail.com>
2021-05-21 10:06:08 +1000

    initial commit

---
+++ apps/app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-eslint-typecheck-rules-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'app';
+}
